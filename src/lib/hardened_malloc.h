af8a5e81d4b83dd77a3fd2e623edc50f548708f1
bemyguestdebug <twizzyontwizzy@gmail.com>
2025-10-06 11:54:57 -0500

    hardened_malloc.h - header gaurds added

--- src/lib/hardened_malloc.h
+++ src/lib/hardened_malloc.h
@@ -1,10 +1,13 @@
+
+#ifndef HARDENED_MALLOC_H
+#define HARDENED_MALLOC_H
+
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <console/console.h>
 #include <cbmem.h>
-
-
 
 #define MAX_HARDENED_ALLOCATIONS 128
 #define CANARY_VALUE 0xBADC0DE
@@ -22,3 +25,5 @@ void hardened_free(void *ptr);
 void *hardened_malloc(size_t size);
 static inline void write_canary(void *ptr);
 static inline uint32_t read_canary(void *ptr);
+
+#endif // HARDENED_MALLOC_H
